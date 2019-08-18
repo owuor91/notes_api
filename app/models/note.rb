8c601e44e625d20145ce29071d4826e39ba8127a
John Owuor <owuor91@gmail.com>
2019-08-19 00:59:29 +0300

    fix rails root

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,7 @@
 class Note < ApplicationRecord
   belongs_to :user
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "",
-  :storage => :s3,:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+  :storage => :s3,:s3_credentials => "Rails.root/config/s3.yml",
                     :bucket => "akirachixnotesapi"
   #validates_attachment :picture, presence: true
   do_not_validate_attachment_file_type :picture
