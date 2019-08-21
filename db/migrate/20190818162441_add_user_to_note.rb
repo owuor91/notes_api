afa5d7a31476e57579fe0ca912dcd1760e88ac24
John Owuor <owuor91@gmail.com>
2019-08-21 13:44:57 +0300

    add user to note

--- db/migrate/20190818162441_add_user_to_note.rb
+++ db/migrate/20190818162441_add_user_to_note.rb
@@ -1,5 +1,5 @@
 class AddUserToNote < ActiveRecord::Migration[6.0]
   def change
-    add_reference :notes, :user, null: false, foreign_key: true, default:0
+    add_reference :notes, :user, null: false, foreign_key: true
   end
 end
