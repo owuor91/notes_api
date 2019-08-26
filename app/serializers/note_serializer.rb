b42213f1e8f4c4d7ab5022f6c3832892a3015756
John Owuor <owuor91@gmail.com>
2019-08-26 22:28:48 +0300

    serialize notes by version

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,9 +1,9 @@
 class NoteSerializer < ActiveModel::VersionSerializer
-  version v1 do
+  version :v1 do
     attributes :id, :title, :noteText
   end
 
-  version v2 do
+  version :v2 do
     version_attributes :v1, with: :picture #, :coordinates
   end
 end
