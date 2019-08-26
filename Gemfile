acb4090c0a12914742abe953c67a715722799fcb
John Owuor <owuor91@gmail.com>
2019-08-26 22:24:49 +0300

    serialize notes by version

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-gem 'active_model_serializers'
+gem 'active_model_version_serializers'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,3 +1,9 @@
-class NoteSerializer < ActiveModel::Serializer
-  attributes :id, :title, :noteText, :picture #, :coordinates
+class NoteSerializer < ActiveModel::VersionSerializer
+  version v1 do
+    attributes :id, :title, :noteText
+  end
+
+  version v2 do
+    version_attributes :v1, with: :picture #, :coordinates
+  end
 end
