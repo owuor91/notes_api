26dea264b3095905711eb988b94f4631ad0faa48
John Owuor <owuor91@gmail.com>
2019-08-26 14:46:20 +0300

    reference env s3 creentials

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,12 @@
 class Note < ApplicationRecord
   belongs_to :user
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "",
-  :storage => :s3,:s3_credentials => "Rails.root/config/s3.yml",
+  :storage => :s3,:s3_credentials => {
+    :bucket => ENV['BUCKET_NAME'],
+    :access_key_id => ENV['ACCESS_KEY_ID'],
+    :secret_access_key => ENV['SECRET_ACCESS_KEY'],
+      s3_region: ENV.['AWS_REGION']
+  },
                     :bucket => "akirachixnotesapi"
   #validates_attachment :picture, presence: true
   do_not_validate_attachment_file_type :picture
